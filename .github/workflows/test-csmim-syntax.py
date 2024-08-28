77b457a090962601859739b1f5e024d34a41daf0
Uwe Salomon <uwe.salomon@diehl.com>
2024-08-28 10:06:33 +0200

    rework tests, add checks for enum-values and parameters
    Signed-off-by: Uwe Salomon <uwe.salomon@diehl.com>

--- .github/workflows/test-csmim-syntax.py
+++ .github/workflows/test-csmim-syntax.py
@@ -5,6 +5,13 @@ import yaml
 import yamale
 
 
+# helper function: loads an object type file
+def load_object_type(typeFile):
+    fd = open(typeFile, "r")
+    return yaml.safe_load(fd)
+
+
+
 # generate test cases for all files in the types folder
 def pytest_generate_tests(metafunc):
     if "typeFile" in metafunc.fixturenames:
@@ -15,6 +22,7 @@ def pytest_generate_tests(metafunc):
         metafunc.parametrize("manufacturerFile", filelist)
 
 
+
 # check the structure of manufacturer files matches the schema
 def test_manufacturers_schema(manufacturerFile):
     schema = yamale.make_schema(".github/workflows/schema_manufacturers.yaml")
@@ -23,17 +31,16 @@ def test_manufacturers_schema(manufacturerFile):
 
 
 # check the structure of type files matches the CSMIM schema
-def test_csmim_schema(typeFile):
+def test_object_type_schema(typeFile):
     schema = yamale.make_schema(".github/workflows/schema_csmim.yaml")
     data = yamale.make_data(typeFile)
     yamale.validate(schema, data)
 
 
 # check the id of all type files matches the filename
-def test_types_id_matches(typeFile):
-    fd = open(typeFile, "r")
-    ty = yaml.safe_load(fd)
-    assert typeFile == "types/" + ty["id"] + ".yaml"
+def test_type_id_matches(typeFile):
+    content = load_object_type(typeFile)
+    assert typeFile == "types/" + content["id"] + ".yaml"
 
 
 # check the id of all manufacturer files matches the filename
@@ -45,33 +52,43 @@ def test_manufacturer_id_matches(manufacturerFile):
 
 # check referenced supertypes exist and are not empty
 def test_supertypes_exist(typeFile):
-    fd = open(typeFile, "r")
-    ty = yaml.safe_load(fd)
-    if "supertypes" in ty:
-        for supertype in ty["supertypes"]:
+    content = load_object_type(typeFile)
+    if "supertypes" in content:
+        for supertype in content["supertypes"]:
             assert len(glob.glob("types/" + supertype + ".yaml")) == 1
 
 
 # recursive helper function to go through the tree of referenced files and check
 # each file is only visited once
-def check_cycle(filename, visited=set()):
-    try:
-        visited.add(filename)
-        fd = open(filename, "r")
-        filecontent = yaml.safe_load(fd)
-        if "supertypes" in filecontent:
-            for next in filecontent["supertypes"]:
-                next = "types/" + next + ".yaml"
-                if next in visited:
-                    return False
-                if not check_cycle(next, visited):
-                    return False
-        return True
-    except Exception as e:
-        print(e)
-        return False
+def has_supertypes_cycle(typeFile, visited):
+    visited.add(typeFile)
+    content = load_object_type(typeFile)
+    if "supertypes" in content:
+        for nextId in content["supertypes"]:
+            nextFile = "types/" + nextId + ".yaml"
+            if nextFile in visited:
+                return True
+            if has_supertypes_cycle(nextFile, visited):
+                return True
+    return False
 
 
 # check there is no cyclic dependencies in referenced supertypes
-def test_supertype_cycles(typeFile):
-    assert check_cycle(typeFile, set()) is True
+def test_supertypes_cycle(typeFile):
+    assert not has_supertypes_cycle(typeFile, set())
+
+
+# check that 'enum-values' are defined for all resources of type 'enum'
+def test_resource_enum_values(typeFile):
+    content = load_object_type(typeFile)
+    for resource in content["resources"]:
+        if resource["type"] == "enum":
+            assert "enum-values" in resource          
+    
+
+# check that 'parameters' are defined for all executable resources
+def test_resource_parameters(typeFile):
+    content = load_object_type(typeFile)
+    for resource in content["resources"]:
+        if resource["mode"] == "x":
+            assert "parameters" in resource          
